688dfc501e336b29db2fd3a7dc7bda6a8446c5e9
vv-p <vvp@protonmail.ch>
2017-05-08 18:29:53 +0300

    Add log level setting to mailer function

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import smtplib
 import arrow
 
 from email.mime.text import MIMEText
+from logging import INFO as LOGGING_INFO, DEBUG as LOGGING_DEBUG
 
 
-def send_on_email(report, subject, mail_from, mail_to, smtp_auth):
+def send_on_email(report, subject, mail_from, mail_to, smtp_auth, log_level=LOGGING_INFO):
     smtp_login, smtp_password = smtp_auth
 
     msg = MIMEText(report.encode('utf-8'), 'html', 'utf-8')
@@ -16,12 +17,17 @@ def send_on_email(report, subject, mail_from, mail_to, smtp_auth):
     msg["To"] = to
 
     s = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-    # s.set_debuglevel(2)  # ToDo: parametrize it
+    if log_level == LOGGING_DEBUG:
+        s.set_debuglevel(1)
     s.login(smtp_login, smtp_password)
     s.sendmail(smtp_login, mail_to, msg.as_string())  # sender address must match authenticated user
 
 
 def get_day_offset():
+    """
+    Get previous working day offset
+    :return: 
+    """
     now = arrow.now()
     offsets = (3, 1, 1, 1, 1, 1, 2)
     return offsets[now.weekday()]
